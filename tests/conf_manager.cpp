cfd72ab4f5eb1a16ec2363bb3295bbd99cac2892
Aleksei Pashinov <pashinov@outlook.com>
2018-01-04 22:53:59 +0300

    Added test for exeption #17

--- tests/conf_manager.cpp
+++ tests/conf_manager.cpp
@@ -2,7 +2,7 @@
 
 #include <configuration_manager.hpp>
 
-TEST(ConfManager, ConfManagerTest) {
+TEST(ConfManager, CorrectFile) {
     std::string filename = "config/server.yaml";
     configuration_manager::instance_ptr()->load_config_file(filename);
 
@@ -20,4 +20,32 @@ TEST(ConfManager, ConfManagerTest) {
 
     std::string pidfile = configuration_manager::instance_ptr()->get_config()->paths_.pid_.pidfile_;
     ASSERT_EQ(pidfile, "/var/run/server.pid");
+}
+
+TEST(ConfManager, WrongFile) {
+
+    std::string server_name;
+    std::string ip_address;
+    std::uint16_t port;
+    std::string logfile;
+    std::string pidfile;
+
+    try
+    {
+        std::string filename = "tests/server_incorrect.yaml";
+        configuration_manager::instance_ptr()->load_config_file(filename);
+        server_name = configuration_manager::instance_ptr()->get_config()->server_name_;
+        ip_address = configuration_manager::instance_ptr()->get_config()->connection_ep_.ip_address_;
+        port = configuration_manager::instance_ptr()->get_config()->connection_ep_.port_;
+        logfile = configuration_manager::instance_ptr()->get_config()->paths_.log_.logfile_;
+        pidfile = configuration_manager::instance_ptr()->get_config()->paths_.pid_.pidfile_;
+    }
+    catch (...)
+    {
+        ASSERT_EQ(server_name, "");
+        ASSERT_EQ(ip_address, "");
+        ASSERT_EQ(port, 0);
+        ASSERT_EQ(logfile, "");
+        ASSERT_EQ(pidfile, "");
+    }
 }
